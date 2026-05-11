596c7d6a27de73eab020e141efc56b915457de27
op-nex <khrariso@gmail.com>
2026-05-11 19:37:03 +0100

    Add files via upload

---
+++ HALLONEST os 0.6.py
@@ -0,0 +1,153 @@
+import os
+import time 
+# from maksoms servers import tuff ahhh calcuator
+def run_tuff_calculator():
+    os.system('cls')
+    print("--- TUFF AHH CALCULATOR LOADED ---")
+    print("hi nooob answer ts to prove urself\n")
+    
+    answer = input("1 + 1 = ")
+    if answer == "2":
+        print("SON...")
+    else:
+        print("U STUPID!")
+
+    anser = input("6 + 7 = ")
+    if anser == "13":
+        print("pretty sure u dont like SCP 67. bruuuuh")
+    elif anser == "67":
+        print("6777777777777777777777 six sevvvvvvvvvvvvvvvven")
+    else:
+        print("no way u didnt answer corectly") 
+
+    pablo = input("9 + 11 = ")
+    if pablo == "20": 
+        print("yah thats correct i think") 
+    elif pablo == "911":
+        print("bro... that is ossama bin ladin ") 
+    else:
+        print("u are so dumb. u should ve been in 9/11") 
+
+    rebonc = input("100 + 1 = (important to do ending 1 or 2): ")
+    if rebonc == "101":
+        print("woooooow... u are so smart") 
+    elif rebonc == "1":
+        print("u passed")
+    elif rebonc == "67":
+        print("blud")
+    
+    else:
+        print("nahh soo dumb.. ima hack u")
+        time.sleep(4)
+        
+        #fakeh hack
+        count = 15
+        while count < 67: 
+            print("u just got haked all ur filles are mine")
+            count += 1
+            time.sleep(0.1)
+        print("nahhhhh u survived ts . howwww")
+        time.sleep(1)
+        print("os tip: emmmm . maybe cuz the version 0.6 has safty prompts")
+        print("get more info from update advanteges )")
+        time.sleep(3)
+        print("vro just get out")
+        time.sleep(1)
+        print("KIKED FROM TUFF AHHH_calculator")
+        time.sleep(6)
+        print("os tip: NAHHHH BRO GOT BANNED FROM A CALCULATOR KKKK")
+        time.sleep(2)
+        print("phhhhh letme give u ur acces to it back")
+        time.sleep(5)
+    print("\nReturning to Hallownest OS ...")
+    time.sleep(3)
+
+# --- os import 
+# from maksom servers import HALLONEST OS V 0.6
+# IF THERE IS A BETTER VERSION IMPORT IT 
+def welcome_screen():
+    os.system('cls')
+    print("=========================================")
+    print("    WELCOME to HALLONEST OS VERSION 0.6  ") 
+    print("=========================================")
+    print("\n[!] الرجاء تسجيل الدخول للمتابعة please log in")
+    
+    user = input(" username اسم المستخدم: ")
+    password = input(" password كلمة المرور: ")
+
+    if (user == "nex" and password == "676767")or (user == "maksom" and password == "nigga"):
+        print("\n[+] searching in maksom AI servers... جاري التحقق")
+        time.sleep(2)
+        print("[✔] access granted تم الدخول بنجاح!")
+        main_menu()
+    else:
+        print("\n[×] thats False. try again lil bro letme recontacte it ")
+        time.sleep(5)
+        print("emmmmm. the maksom servers are trash . u need to wait ")
+        time.sleep(1)
+        print("concequnces of putting fake name ")
+        time.sleep(3)
+        print("yo M sersvers responded finally")
+        time.sleep(1)
+        welcome_screen()
+def main_menu():
+    while True: # FINALY USING MAKSOM SERVERS TO DEBUG KKK
+        os.system('cls')
+        print("--- HALLONEST MAIN MENU ---")
+        print("waiting for orders...")
+        print("1. Open tuff ahh calculator (RECOMMENDED)")
+        print("2. Enter maksom AI app")
+        print("3. Exit OS")
+        print("4. update advantegs and os ")
+        print("\nWARNING: DONT PICK UNDISPLAYED NUMBERS OR U BANNED")
+        
+        choice = input("\npick a number: ")
+        
+        if choice == "1":
+            run_tuff_calculator() # use maksom AI servers kkkk
+        elif choice == "2":
+            print("asking M servers for access... pls wait")
+            time.sleep(3)
+            print("sry. maksom AI servers are in the abyss")
+            print("try the local host in instagram")
+            time.sleep(2)
+        elif choice == "3":
+            print("closing system")
+            time.sleep(2)
+            print("fun fact the servers are hosted by Maksom (the worst company kkkk)")
+            time.sleep(2)
+            print("see u soon...")
+            time.sleep(25)
+            print("emmmmmmm")
+            print("why are you waiting")
+            time.sleep(3)
+            print("i belive u want to re log . ")
+            time.sleep(2)
+            print("fine........")
+            welcome_screen()
+        elif choice == "4":
+            print("os version...")
+            time.sleep(2)
+            print("HALLONEST OS V 0.6 ")
+            print("update not available sadly")
+            time.sleep(5)
+            print("update advanteges : ")
+            print("colabe with maksom servers from alonia, the abbys and maksom AI locale host from insta")
+            print("new ability. u can use the OS multipul times without log in cuz the M servers safty is improved ")
+            print("using the maksom servers . tuff ahh calculator is available without downloding it ")
+            print("moving to desktop in 15s")
+            time.sleep(15)
+            print("move to looby")
+
+        else:
+            print("TOO BAD U WILL BE BANND FROM UR OWN PC KKKK")
+            time.sleep(3)
+            print("just kidding bro kkkk")
+            time.sleep(1)
+            print("try again")
+            time.sleep(2)
+
+# os ask task
+if __name__ == "__main__":
+    welcome_screen()
+    
